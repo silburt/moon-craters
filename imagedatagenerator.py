1c326c4f8fa7494af4ef5a61d2e915fe0ae157db
silburt <silburt@astro.utoronto.ca>
2017-04-14 09:50:06 -0400

    change

--- imagedatagenerator.py
+++ imagedatagenerator.py
@@ -1,3 +1,5 @@
+#This code takes an image and manipulates it using the keras preprocessing package.
+
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 import numpy as np
 import os

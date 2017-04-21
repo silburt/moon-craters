042bcc6212cd7559cfb7ec30b9a59192466607ae
silburt <silburt@astro.utoronto.ca>
2017-04-21 09:19:46 -0400

    changes

--- moon_vgg16_1.2.2_test.py
+++ moon_vgg16_1.2.2_test.py
@@ -75,9 +75,9 @@ def read_and_normalize_data(path, img_width, img_height, data_flag):
 ########################################################################
 #Following https://github.com/fchollet/keras/blob/master/keras/applications/vgg16.py 
 def vgg16(n_classes,im_width,im_height,learn_rate,lmbda,dropout):
-    n_filters = 32          #vgg16 uses 64
+    n_filters = 64          #vgg16 uses 64
     n_blocks = 3            #vgg16 uses 5
-    n_dense = 1024          #vgg16 uses 4096
+    n_dense = 512           #vgg16 uses 4096
 
     #first block
     print('Making VGG model...')
@@ -88,17 +88,17 @@ def vgg16(n_classes,im_width,im_height,learn_rate,lmbda,dropout):
 
     #subsequent blocks
     for i in np.arange(1,n_blocks):
-        n_filters_ = np.min((n_filters*2**i, 512))                      #maximum of 512 filters in vgg16
+        n_filters_ = np.min((n_filters*2**i, 512))                          #maximum of 512 filters in vgg16
         model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
         model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Flatten())
     model.add(Dropout(dropout))
-    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))
+    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))   #biggest memory sink
     model.add(Dropout(dropout))
     model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))
-    model.add(Dense(n_classes, activation='relu', name='predictions'))   #relu/regression output
+    model.add(Dense(n_classes, activation='relu', name='predictions'))      #relu/regression output
 
     #optimizer = SGD(lr=learn_rate, momentum=0.9, decay=0.0, nesterov=True)
     optimizer = Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)

334d366ad9d591228d5485564cee44edb10c68d6
silburt <silburt@astro.utoronto.ca>
2017-05-08 15:28:35 -0400

    change

--- moon_vgg16_1.2.2_fcn2.py
+++ moon_vgg16_1.2.2_fcn2.py
@@ -81,36 +81,30 @@ def read_and_normalize_data(path, img_width, img_height, data_flag):
 def FCN_model(im_width,im_height,learn_rate,lmbda):
     print('Making VGG16-style Fully Convolutional Network model...')
     n_filters = 32          #vgg16 uses 64
-    n_blocks = 4            #vgg16 uses 5
-    n_dense = 256          #vgg16 uses 4096
-    upsample = im_height    #upsample scale - factor to get back to img_height, im_width
-
+    n_blocks = 3            #vgg16 uses 5
+    n_dense = 512           #vgg16 uses 4096
+    
     #first block
+    print('Making VGG model...')
     model = Sequential()
     model.add(Conv2D(n_filters, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda), input_shape=(im_width,im_height,3)))
     model.add(Conv2D(n_filters, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
-    '''
     model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     
     #subsequent blocks
     for i in np.arange(1,n_blocks):
-        n_filters_ = np.min((n_filters*2**i, 512))
+        n_filters_ = np.min((n_filters*2**i, 512))                          #maximum of 512 filters in vgg16
         model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
         model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
-        if i==1:
-            model.add(MaxPooling2D(pool_size=(2, 2), strides=(3, 3)))
-        else:
-            model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    #reinterpreted FC layers - http://cs231n.github.io/convolutional-networks/#convert
-    model.add(Conv2D(n_dense, nb_row=12, nb_col=12, activation='relu', border_mode='valid', W_regularizer=l2(lmbda), name='fc1')) #filter dim = dim of previous layer
-    model.add(Conv2D(n_dense, nb_row=1, nb_col=1, activation='relu', border_mode='valid', W_regularizer=l2(lmbda), name='fc2'))
-    
-    #Upsample and create mask
-    model.add(UpSampling2D(size=(upsample, upsample)))'''
-    model.add(Conv2D(1, nb_row=upsample, nb_col=upsample, activation='relu', border_mode='same', W_regularizer=l2(lmbda), name='output'))
-    model.add(Reshape((im_width,im_height)))
-    
+    model.add(Flatten())
+    model.add(Dropout(dropout))
+    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))   #biggest memory sink
+    model.add(Dropout(dropout))
+    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))
+    model.add(Dense(n_classes, activation='relu', name='predictions'))      #relu/regression output
+
     #optimizer = SGD(lr=learn_rate, momentum=0.9, decay=0.0, nesterov=True)
     optimizer = Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
     model.compile(loss='mae', optimizer=optimizer, metrics=['accuracy'])

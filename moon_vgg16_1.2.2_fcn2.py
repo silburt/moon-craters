c3b0b592e54eb9f189ecf9f273c15d13258a28b8
silburt <silburt@astro.utoronto.ca>
2017-05-08 15:34:01 -0400

    changes

--- moon_vgg16_1.2.2_fcn2.py
+++ moon_vgg16_1.2.2_fcn2.py
@@ -83,29 +83,14 @@ def FCN_model(im_width,im_height,learn_rate,lmbda):
     n_filters = 32          #vgg16 uses 64
     n_blocks = 3            #vgg16 uses 5
     n_dense = 512           #vgg16 uses 4096
-    dropout = 0
-    n_classes=1
     
     #first block
-    print('Making VGG model...')
     model = Sequential()
     model.add(Conv2D(n_filters, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda), input_shape=(im_width,im_height,3)))
     model.add(Conv2D(n_filters, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-    
-    #subsequent blocks
-    for i in np.arange(1,n_blocks):
-        n_filters_ = np.min((n_filters*2**i, 512))                          #maximum of 512 filters in vgg16
-        model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
-        model.add(Conv2D(n_filters_, nb_row=3, nb_col=3, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
-        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
-    model.add(Flatten())
-    model.add(Dropout(dropout))
-    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))   #biggest memory sink
-    model.add(Dropout(dropout))
-    model.add(Dense(n_dense, activation='relu', W_regularizer=l2(lmbda)))
-    model.add(Dense(n_classes, activation='relu', name='predictions'))      #relu/regression output
+    model.add(Conv2D(1, nb_row=300, nb_col=300, activation='relu', border_mode='same', W_regularizer=l2(lmbda)))
+    model.add(Reshape((300,300)))
 
     #optimizer = SGD(lr=learn_rate, momentum=0.9, decay=0.0, nesterov=True)
     optimizer = Adam(lr=learn_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)

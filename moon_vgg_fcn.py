41049f2953d114f21624bd325dcd488050559eb1
silburt <silburt@astro.utoronto.ca>
2017-05-15 09:54:25 -0400

    changes

--- moon_vgg_fcn.py
+++ moon_vgg_fcn.py
@@ -28,7 +28,7 @@ from keras import __version__ as keras_version
 from keras import backend as K
 K.set_image_dim_ordering('tf')
 
-import utils.make_density_map2d as mdm
+import utils.make_density_map as mdm
 
 #####################
 #load/read functions#

4d41945e4f8376e7572bbbca9b0216eef2be4240
silburt <silburt@astro.utoronto.ca>
2017-05-24 15:24:39 -0400

    change

--- utils/make_density_map.py
+++ utils/make_density_map.py
@@ -1,6 +1,14 @@
+#!/usr/bin/env python
+"""Density and Mask Making
+
+Scripts for determining centroid density maps and segmentation masks as targetes for CNN.  See docstrings of functions for further help.
+"""
+
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from PIL import Image
+import cv2
 
 from scipy.spatial import cKDTree as kd
 
@@ -19,7 +27,7 @@ def gkern(l=5, sig=1.):
 
 
 # https://stackoverflow.com/questions/10031580/how-to-write-simple-geometric-shapes-into-numpy-arrays
-def circlemaker(r=10., rings=False):
+def circlemaker(r=10.):
     """
     Creates circle mask of radius r.
     """
@@ -28,12 +36,38 @@ def circlemaker(r=10., rings=False):
     rhext = int(r) + 1
 
     xx, yy = np.mgrid[-rhext:rhext + 1, -rhext:rhext + 1]
-    if rings == True:
-        circle = np.abs((xx**2 + yy**2) - r**2) <= 3*r
-    else:
-        circle = (xx**2 + yy**2) <= r**2
+    circle = (xx**2 + yy**2) <= r**2
 
     return circle.astype(float)
+
+# http://docs.opencv.org/2.4/modules/core/doc/drawing_functions.html#circle
+# Though that autodoc is terrible, and should be supplemented with
+# http://docs.opencv.org/3.1.0/dc/da5/tutorial_py_drawing_functions.html
+# and (file that defines static void Circle(...))
+# https://github.com/opencv/opencv/blob/05b15943d6a42c99e5f921b7dbaa8323f3c042c6/modules/imgproc/src/drawing.cpp
+def ringmaker(r=10., dr=1):
+    """
+    Creates ring of radius r and thickness dr.
+
+    Parameters
+    ----------
+    r : float
+        Ring radius
+    dr : int
+        Ring thickness (cv2.circle requires int)
+    """
+
+    # mask grid extent (dr/2 +1 to ensure we capture ring width
+    # and radius); same philosophy as above
+    rhext = int(np.ceil(r + dr/2.)) + 1
+
+    # cv2.circle requires integer radius
+    mask = np.zeros([2*rhext + 1, 2*rhext + 1], np.uint8)
+
+    # Generate ring
+    ring = cv2.circle(mask, (rhext,rhext), int(np.round(r)), 1, thickness=dr)
+
+    return ring.astype(float)
 
 
 def get_merge_indices(cen, imglen, ks_h, ker_shp):
@@ -64,16 +98,16 @@ def get_merge_indices(cen, imglen, ks_h, ker_shp):
     return [img_l, img_r, g_l, g_r]
 
 
-def make_density_map(craters, imgshape, kernel=None, k_support = 8, k_sig=4., knn=10, 
-                        beta=0.3, kdict={}):
+def make_density_map(craters, img, kernel=None, k_support = 8, k_sig=4., knn=10, 
+                        beta=0.3, kdict={}, truncate=True):
     """Makes Gaussian kernel density maps.
 
     Parameters
     ----------
     craters : pandas.DataFrame
         craters dataframe that includes pixel x and y columns
-    imgshape : listlike
-        Image dimensions [y, x], i.e. output of img.shape
+    img : numpy.ndarray
+        original image; assumes colour channel is last axis (tf standard)
     kernel : function, "knn" or None
         If a function is inputted, function must return an array of 
         length craters.shape[0].  If "knn",  uses variable kernel with 
@@ -93,30 +127,38 @@ def make_density_map(craters, imgshape, kernel=None, k_support = 8, k_sig=4., kn
         is 0.3.
     kdict : dict
         If kernel is custom function, dictionary of arguments passed to kernel.
+    truncate : bool
+        If True, truncate mask where image truncates
     """
 
     # Load blank density map
+    imgshape = img.shape[:2]
     dmap = np.zeros(imgshape)
+
+    # Get number of craters
+    N_ctrs = craters.shape[0]
 
     # Obtain gaussian kernel sigma values
     # callable checks if kernel is function
     if callable(kernel):
         sigma = kernel(**kdict)
+    # If knn is used
     elif kernel == "knn":
-        kdt = kd(craters[["x","y"]].as_matrix(), leafsize=10)
-        dnn = kdt.query(craters[["x","y"]].as_matrix(), \
-                                k=knn + 1)[0][:, 1:].mean(axis=1)
+        # If we have more than 1 crater, select either nearest 11 or N_ctrs
+        # neighbours, whichever is closer
+        if N_ctrs > 1:
+            kdt = kd(craters[["x","y"]].as_matrix(), leafsize=10)
+            dnn = kdt.query(craters[["x","y"]].as_matrix(), \
+                                    k=min(N_ctrs, knn + 1))[0][:, 1:].mean(axis=1)
+        # Otherwise, assume there are craters "offscreen" half an image away
+        else:
+            dnn = 0.5*imgshape[0]*np.ones(1)
         sigma = beta*dnn
     else:
-        sigma = k_sig*np.ones(craters.shape[0])
+        sigma = k_sig*np.ones(N_ctrs)
 
-
-    #kdt = kd(craters[["x","y"]].as_matrix(), leafsize=10)
-    #dnn = kdt.query(craters[["x","y"]].as_matrix(), k=11)[0][:, 1:].mean(axis=1)
-    #ker_sigma = 0.3*dnn
-    #ker_sigma = 4*np.ones(dnn.shape)
-
-    for i in range(craters.shape[0]):
+    # Gaussian adding loop
+    for i in range(N_ctrs):
         cx = int(craters["x"][i]); cy = int(craters["y"][i])
 
         # A bit convoluted, but ensures that kernel_support
@@ -135,43 +177,58 @@ def make_density_map(craters, imgshape, kernel=None, k_support = 8, k_sig=4., kn
         # Add kernel to image
         dmap[imyl:imyr, imxl:imxr] += kernel[gyl:gyr, gxl:gxr]
 
+    # Removes
+    if truncate:
+        if img.ndim == 3:
+            dmap[img[:,:,0] == 0] = 0
+        else:
+            dmap[img == 0] = 0
+
     return dmap
 
-def make_mask(craters, img, binary=True, truncate=True, rings=False):
-    """Makes crater mask binary image.
-        
-        Parameters
-        ----------
-        craters : pandas.DataFrame
+
+def make_mask(craters, img, binary=True, rings=False, 
+                                ringwidth=1, truncate=True):
+    """Makes crater mask binary image (does not yet consider crater overlap).
+
+    Parameters
+    ----------
+    craters : pandas.DataFrame
         craters dataframe that includes pixel x and y columns
-        img : 3D numpy array
-        original image.
-        binary : bool
+    img : numpy.ndarray
+        original image; assumes colour channel is last axis (tf standard)
+    binary : bool
         If True, returns a binary image of crater masks
-        truncate : bool
+    rings : bool
+        If True, mask uses hollow rings rather than filled circles
+    ringwiddth : int
+        If rings is True, ringwidth sets the width (dr) of the ring.
+        
+    truncate : bool
         If True, truncate mask where image truncates
-        rings : bool
-        If True, make rings instead of circles.
-        """
-    
+    """
+
     # Load blank density map
-    imgshape = (img.shape[0], img.shape[1])     #Image dimensions [y, x], i.e. output of img.shape
+    imgshape = img.shape[:2]
+    dmap = np.zeros(imgshape)
     cx, cy = craters["x"].values.astype('int'), craters["y"].values.astype('int')
     radius = craters["Diameter (pix)"].values / 2.
-    
-    dmap = np.zeros(imgshape)
+
     for i in range(craters.shape[0]):
-        kernel = circlemaker(radius[i], rings)
+        if rings:
+            kernel = ringmaker(r=radius[i], dr=ringwidth)
+        else:
+            kernel = circlemaker(r=radius[i])
         # "Dummy values" so we can use get_merge_indices
         kernel_support = kernel.shape[0]
         ks_half = kernel_support // 2
-        
+
         # Calculate indices on image where kernel should be added
         [imxl, imxr, gxl, gxr] = get_merge_indices(cx[i], imgshape[1],
-                                                   ks_half, kernel_support)
+                                                    ks_half, kernel_support)
         [imyl, imyr, gyl, gyr] = get_merge_indices(cy[i], imgshape[0],
-                                                   ks_half, kernel_support)
-                                                   
+                                                    ks_half, kernel_support)
+
         # Add kernel to image
         dmap[imyl:imyr, imxl:imxr] += kernel[gyl:gyr, gxl:gxr]
     
@@ -179,76 +236,12 @@ def make_mask(craters, img, binary=True, truncate=True, rings=False):
         dmap = (dmap > 0).astype(float)
     
     if truncate:
-        dmap[img[:,:,0] == 0] = 0
+        if img.ndim == 3:
+            dmap[img[:,:,0] == 0] = 0
+        else:
+            dmap[img == 0] = 0
     
-    return dmap
-
-#A.S. defined function
-def make_circlering_mask(craters, img, binary=True, truncate=True):
-    """Makes circlering masks, i.e. crater edge -> ring = 1, crater itself -> circle = 0.5
-
-    Parameters
-    ----------
-    craters : pandas.DataFrame
-        craters dataframe that includes pixel x and y columns
-    img : 3D numpy array
-        original image. 
-    binary : bool
-        If True, returns a binary image of crater masks
-    truncate : bool
-        If True, truncate mask where image truncates
-    rings : bool
-        If True, make rings instead of circles.
-    """
-
-    # Load blank density map
-    imgshape = (img.shape[0], img.shape[1])     #Image dimensions [y, x], i.e. output of img.shape
-    cx, cy = craters["x"].values.astype('int'), craters["y"].values.astype('int')
-    radius = craters["Diameter (pix)"].values / 2.
-    dmap_rings, dmap_circles = np.zeros(imgshape), np.zeros(imgshape)
-
-    #generate circles
-    for i in range(craters.shape[0]):
-        kernel = circlemaker(radius[i], 0)
-        # "Dummy values" so we can use get_merge_indices
-        kernel_support = kernel.shape[0]
-        ks_half = kernel_support // 2
-        
-        # Calculate indices on image where kernel should be added
-        [imxl, imxr, gxl, gxr] = get_merge_indices(cx[i], imgshape[1],
-                                                   ks_half, kernel_support)
-        [imyl, imyr, gyl, gyr] = get_merge_indices(cy[i], imgshape[0],
-                                                   ks_half, kernel_support)
-                                                   
-        # Add kernel to image
-        dmap_circles[imyl:imyr, imxl:imxr] += kernel[gyl:gyr, gxl:gxr]
-    
-    if binary:
-        dmap_circles[dmap_circles > 0] = 0.5
-    if truncate:
-        dmap_circles[img[:,:,0] == 0] = 0
-
-    #generate rings
-    for i in range(craters.shape[0]):
-        kernel = circlemaker(radius[i], 1)
-        # "Dummy values" so we can use get_merge_indices
-        kernel_support = kernel.shape[0]
-        ks_half = kernel_support // 2
-        
-        # Calculate indices on image where kernel should be added
-        [imxl, imxr, gxl, gxr] = get_merge_indices(cx[i], imgshape[1],
-                                                   ks_half, kernel_support)
-        [imyl, imyr, gyl, gyr] = get_merge_indices(cy[i], imgshape[0],
-                                                   ks_half, kernel_support)
-        # Add kernel to image
-        dmap_rings[imyl:imyr, imxl:imxr] += kernel[gyl:gyr, gxl:gxr]
-
-        if binary:
-            dmap_rings[dmap_rings > 0] = 1
-        if truncate:
-            dmap_rings[img[:,:,0] == 0] = 0
-
-    dmap = dmap_circles + dmap_rings
-    dmap[dmap > 1] = 1
+    #add centroids to image
+    #dmap[cy,cx] = 2
 
     return dmap

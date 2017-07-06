8ece0c5d7a64b7af84931537a2ff0f384b24e8be
silburt <silburt@astro.utoronto.ca>
2017-07-06 10:22:47 -0300

    edit

--- rings_analyze_remote.py
+++ rings_analyze_remote.py
@@ -4,11 +4,21 @@ import numpy as np
 from keras.models import load_model
 from keras import backend as K
 from utils.rescale_invcolor import rescale_and_invcolor
+################
+#Loss Functions#
+########################################################################
+def weighted_binary_XE(y_true, y_pred):
+    #sum total number of 1s and 0s in y_true
+    total_ones = tf.reduce_sum(y_true)
+    total_zeros = tf.reduce_sum(tf.to_float(tf.equal(y_true, tf.zeros_like(y_true))))
+    result = K.binary_crossentropy(y_pred, y_true)
+    weights = y_true * total_zeros*1.0/(total_zeros + total_ones)
+    return K.mean(result*weights + result, axis=-1)
 
 ##############
 #Main Routine#
 ########################################################################
-def predict_targets(dir,inv_color,rescale,n_pred_samples,offset,models):
+def predict_targets(dir,inv_color,rescale,n_pred_samples,offset,models,custom_loss):
     #static arguments
     dim = 256               #image dimensions, assuming square images. Should not change
     
@@ -31,7 +41,10 @@ def predict_targets(dir,inv_color,rescale,n_pred_samples,offset,models):
 
     print "Generating predictions."
     for m in models:
-        model = load_model('%s'%m)
+        if custom_loss == 1:
+            model = load_model('%s'%m, custom_objects={'weighted_binary_XE': weighted_binary_XE})
+        else:
+            model = load_model('%s'%m)
         target_pred = model.predict(test_data[offset:(n_pred_samples+offset)].astype('float32'))
         
         #dimensions go data, ground_truth targets, predicted targets
@@ -53,8 +66,9 @@ if __name__ == '__main__':
     rescale = 1             #rescale images to increase contrast (**must be same setting as what was used for the model(s)**)
     n_pred_samples = 20     #number of test images to predict on
     offset = 0              #index offset to start predictions at in test array
+    custom_loss = 0        #custom loss
     models = ['models/unet_s256_rings_FL3_weightedpred.h5']
     
-    predict_targets(dir,inv_color,rescale,n_pred_samples,offset,models)
+    predict_targets(dir,inv_color,rescale,n_pred_samples,offset,models,custom_loss)
     
 

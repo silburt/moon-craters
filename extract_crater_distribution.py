0c38a52d76bc23f391763283b64ff9dccd5e3cc7
silburt <silburt@astro.utoronto.ca>
2017-07-20 14:32:14 -0400

    bug

--- extract_crater_distribution.py
+++ extract_crater_distribution.py
@@ -37,7 +37,7 @@ def get_crater_dist(dir,type,n_imgs,modelname,inv_color,rescale):
     master_img_height_pix = 20000.  #number of pixels for height
     master_img_height_lat = 180.    #degrees used for latitude
     r_moon = 1737                   #radius of the moon (km)
-    olen = 256                      #output length (pixels)
+    dim = 256                       #output length (pixels)
     P = cPickle.load(open('%s/lolaout_%s.p'%(dir,type), 'r'))
     
     # get data
@@ -68,7 +68,7 @@ def get_crater_dist(dir,type,n_imgs,modelname,inv_color,rescale):
     for i in range(n_imgs):
         coords = template_match_target(pred[i])
         img_pix_height = P[test_id[i]]['box'][2] - P[test_id[i]]['box'][0]
-        pix_to_km = (master_img_height_lat/master_img_height_pix)*(np.pi/180)*(img_pix_height/olen)*r_moon
+        pix_to_km = (master_img_height_lat/master_img_height_pix)*(np.pi/180)*(img_pix_height/dim)*r_moon
         _,_,radii = zip(*coords*pix_to_km)
         master_crater_dist += list(radii)
 

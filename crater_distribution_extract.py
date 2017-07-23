189c2e3154093dc90ecf1670ed8742d058b4e62a
silburt <silburt@astro.utoronto.ca>
2017-07-23 09:14:26 -0700

    edit

--- crater_distribution_extract.py
+++ crater_distribution_extract.py
@@ -1,3 +1,5 @@
+# This code is typically run on p8t03/04 on scinet.
+
 import numpy as np
 import cPickle
 import cv2
@@ -32,12 +34,14 @@ def read_and_normalize_data(path, dim, data_type):
     print('%s shape:'%data_type, data.shape)
     return data, target, id_
 
-def get_crater_dist(dir,type,n_imgs,modelpath,inv_color,rescale,ground_truth_dist):
+def get_crater_dist(dir,type,n_imgs,modelpath,inv_color,rescale,ground_truth_only):
+    pred_crater_dist, GT_crater_dist = [], []
+    
     # properties of the dataset, shouldn't change (unless you use a different dataset)
     master_img_height_pix = 20000.  #number of pixels for height
     master_img_height_lat = 180.    #degrees used for latitude
     r_moon = 1737                   #radius of the moon (km)
-    dim = 256                       #output length (pixels)
+    dim = 256                       #image dimension (pixels, assume dim=height=width)
     P = cPickle.load(open('%s/lolaout_%s.p'%(dir,type), 'r'))
     
     # get data
@@ -55,43 +59,46 @@ def get_crater_dist(dir,type,n_imgs,modelpath,inv_color,rescale,ground_truth_dis
         np.save('%s%s_id.npy'%(path[type],type),id)
 
     data, target, id = data[:n_imgs], target[:n_imgs], id[:n_imgs]
-    if inv_color==1 or rescale==1:
-        print "inv_color=%d, rescale=%d, processing data"%(inv_color, rescale)
-        data = rescale_and_invcolor(data, inv_color, rescale)
 
-    # generate model predictions and fit template match
-    model = load_model(modelpath)
-    pred = model.predict(data.astype('float32'))
+    if ground_truth_only == 0:
+        if inv_color==1 or rescale==1:
+            print "inv_color=%d, rescale=%d, processing data"%(inv_color, rescale)
+            data = rescale_and_invcolor(data, inv_color, rescale)
+        
+        # generate model predictions and fit template match
+        model = load_model(modelpath)
+        pred = model.predict(data.astype('float32'))
 
-    # extract crater distribution
-    pred_crater_dist = []
-    print "Extracting crater radius distribution of %d %s files."%(n_imgs,type)
-    for i in range(len(pred)):
-        coords = template_match_target(pred[i])
-        img_pix_height = P[id[i]]['box'][2] - P[id[i]]['box'][0]
-        pix_to_km = (master_img_height_lat/master_img_height_pix)*(np.pi/180)*(img_pix_height/dim)*r_moon
-        if len(coords) >= 1:
-            _,_,radii = zip(*coords*pix_to_km)
-            pred_crater_dist += list(radii)
+        # extract crater distribution
+        print "Extracting crater radius distribution of %d %s files."%(n_imgs,type)
+        for i in range(len(pred)):
+            coords = template_match_target(pred[i])
+            img_pix_height = P[id[i]]['box'][2] - P[id[i]]['box'][0]
+            pix_to_km = (master_img_height_lat/master_img_height_pix)*(np.pi/180)*(img_pix_height/dim)*r_moon
+            if len(coords) >= 1:
+                _,_,radii = zip(*coords*pix_to_km)
+                pred_crater_dist += list(radii)
 
-    GT_crater_dist = []
-    if ground_truth_dist == 1:
-        print "Getting ground truth crater distribution."
-        minrad, maxrad = 3, 75    #min/max radius (in pixels) required to include crater in target
-        cutrad = 0.5              #0-1 range, if x+cutrad*r > img_width, remove, i.e. exclude craters ~half gone from image
-        for id_ in id:
-            csv = pd.read_csv('%slola_%s.csv'%(path[type],str(id_).zfill(5)))
-            csv = csv[(csv['Diameter (pix)'] < 2*maxrad) & (csv['Diameter (pix)'] > 2*minrad)]
-            csv = csv[(csv['x']+cutrad*csv['Diameter (pix)']/2 <= dim)]
-            csv = csv[(csv['y']+cutrad*csv['Diameter (pix)']/2 <= dim)]
-            csv = csv[(csv['x']-cutrad*csv['Diameter (pix)']/2 > 0)]
-            csv = csv[(csv['y']-cutrad*csv['Diameter (pix)']/2 > 0)]
-            GT_radius = csv['Diameter (km)'].values/2
-            GT_crater_dist += list(GT_radius)
+        pred_crater_dist = np.asarray(pred_crater_dist)
+        np.save('%s%s_predcraterdist_n%d.npy'%(path[type],type,n_imgs),pred_crater_dist)
 
-    pred_crater_dist, GT_crater_dist = np.asarray(pred_crater_dist), np.asarray(GT_crater_dist)
-    np.save('%s%s_predcraterdist_n%d.npy'%(path[type],type,n_imgs),pred_crater_dist)
-    np.save('%s%s_GTcraterdist_n%d.npy'%(path[type],type,n_imgs),GT_crater_dist)
+    # Generate csv dist
+    # hyperparameters
+    minrad, maxrad = 3, 75    #min/max radius (in pixels) required to include crater in target
+    cutrad = 0.85             #0-1 range, if x+cutrad*r > dim, remove, higher cutrad = larger % of circle required
+    print "Getting ground truth crater distribution."
+    for id_ in id:
+        csv = pd.read_csv('%slola_%s.csv'%(path[type],str(id_).zfill(5)))
+        csv = csv[(csv['Diameter (pix)'] < 2*maxrad) & (csv['Diameter (pix)'] > 2*minrad)]
+        csv = csv[(csv['x']+cutrad*csv['Diameter (pix)']/2 <= dim)]
+        csv = csv[(csv['y']+cutrad*csv['Diameter (pix)']/2 <= dim)]
+        csv = csv[(csv['x']-cutrad*csv['Diameter (pix)']/2 > 0)]
+        csv = csv[(csv['y']-cutrad*csv['Diameter (pix)']/2 > 0)]
+        GT_radius = csv['Diameter (km)'].values/2
+        GT_crater_dist += list(GT_radius)
+
+    GT_crater_dist = np.asarray(GT_crater_dist)
+    np.save('%s%s_GTcraterdist_n%d_cutrad0.85.npy'%(path[type],type,n_imgs),GT_crater_dist)
     return pred_crater_dist, GT_crater_dist
 
 if __name__ == '__main__':
@@ -99,10 +106,10 @@ if __name__ == '__main__':
     dir = 'datasets/rings'  #location of Train_rings/, Dev_rings/, Test_rings/ folders. Exclude final '/' in path.
     type = 'test'           #what to get crater distribution of: train, dev, test
     n_imgs = 30016          #number of images to use for getting crater distribution.
-    ground_truth_dist = 1   #get ground truth crater distribution too (from csvs)
+    ground_truth_only = 1   #get ground truth crater distribution only (from csvs), do not generate predictions
     
     modelpath = 'models/unet_s256_rings_nFL96.h5'
     inv_color = 1           #**must be same setting as what model was trained on**
     rescale = 1             #**must be same setting as what model was trained on**
 
-    pred_crater_dist, GT_crater_dist = get_crater_dist(dir,type,n_imgs,modelpath,inv_color,rescale,ground_truth_dist)
+    pred_crater_dist, GT_crater_dist = get_crater_dist(dir,type,n_imgs,modelpath,inv_color,rescale,ground_truth_only)
---
+++ crater_distribution_plot.py
@@ -0,0 +1,16 @@
+#plot the results on local machine
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+pred = np.load('datasets/rings/Test_rings/test_predcraterdist_n30016.npy')
+truth = np.load('datasets/rings/Test_rings/test_GTcraterdist_n30016.npy')
+
+norm = True
+
+plt.hist(pred, 30, range=[min(truth),max(truth)], normed=norm, label='pred')
+#plt.hist(pred, 30, normed=norm, label='pred')
+plt.hist(truth, 30, normed=norm, alpha=0.5, label='ground truth')
+plt.legend()
+plt.savefig('output_dir/images/crater_dist_comp_samemin_norm.png')
+plt.show()

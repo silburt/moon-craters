8e81eb0ddf7df2524ee70b1e31c6493223f994a0
silburt <silburt@astro.utoronto.ca>
2017-08-24 22:28:46 -0400

    bug

--- crater_distribution_unique.py
+++ crater_distribution_unique.py
@@ -9,10 +9,10 @@ def extract_unique(pred, unique_thresh2, id, P):
     master_img_height_lat = 180.    #degrees used for latitude
     r_moon = 1737.4                 #radius of the moon (km)
     dim = 256                       #image dimension (pixels, assume dim=height=width)
-
+    
     pred_crater_dist = np.empty([0,3])
+    N_matches_tot = 0
     for i in range(len(pred)):
-        print i, len(pred_crater_dist)
         coords = template_match_target(pred[i])
         if len(coords) > 0:
             P_ = P[id[i]]
@@ -23,6 +23,7 @@ def extract_unique(pred, unique_thresh2, id, P):
             long_deg = P_['llbd'][0] + (P_['llbd'][1]-P_['llbd'][0])*(long_pix/float(dim))
             lat_deg = P_['llbd'][3] - (P_['llbd'][3]-P_['llbd'][2])*(lat_pix/float(dim))
             tuple_ = np.column_stack((long_deg,lat_deg,radii_km))
+            N_matches_tot += len(coords)
             
             #only add unique (non-duplicate) values to the master pred_crater_dist
             if len(pred_crater_dist) > 0:
@@ -36,20 +37,21 @@ def extract_unique(pred, unique_thresh2, id, P):
                 pred_crater_dist = np.concatenate((pred_crater_dist,tuple_))
 
     np.save('%s/test_uniquedist_ut%.1e.npy'%(dir,unique_thresh2),pred_crater_dist)
+    print "Total Number of Matches for %f: %d"%(unique_thresh2,N_matches_tot)
     print ""
 
 if __name__ == '__main__':
     #arrays = (long, lat, radii)
     dir = 'datasets/rings/Test_rings'
-    pred = np.load('%s/test_modelpreds_n1000_new.npy'%dir)
+    pred = np.load('%s/test_modelpreds_n10016_new.npy'%dir)
     id = np.load('%s/test_id.npy'%dir)
     P = cPickle.load(open('%s/lolaout_test.p'%dir, 'r'))
 
     #unique_thresh2 = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4, 1e-3]
-    unique_thresh2 = [5,1,0.1]
+    unique_thresh2 = [1,0.1,1e-2,1e-3]
     for ut2 in unique_thresh2:
 #        print "extracting unique ground truth craters, unique_thresh2=%.2f"%ut2
 #        GT = extract_unique(truth, ut2, dir, output_nametruth)
 
         print "extracting unique predicted craters, unique_thresh2=%.2f"%ut2
-        pred = extract_unique(pred, ut2, id, P)
+        extract_unique(pred, ut2, id, P)

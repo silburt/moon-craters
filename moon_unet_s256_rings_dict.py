541b5671f52e9c35755456974428153372eaf2ba
silburt <silburt@astro.utoronto.ca>
2017-11-05 20:44:54 -0500

    edit

--- moon_unet_s256_rings_dict.py
+++ moon_unet_s256_rings_dict.py
@@ -23,7 +23,7 @@ from keras.models import Sequential, Model
 from keras.layers.core import Dense, Dropout, Flatten, Reshape
 from keras.layers import AveragePooling2D, merge, Input
 from keras.layers.convolutional import Convolution2D, MaxPooling2D, UpSampling2D
-from keras.objectives import binary_crossentropy
+#from keras.objectives import binary_crossentropy
 from keras.regularizers import l2
 from keras.models import load_model
 
@@ -67,6 +67,14 @@ def get_param_i(param,i):
         return param[i]
     else:
         return param[0]
+
+def binary_crossentropy(target, output):
+    _EPSILON = 10e-8
+    _epsilon = tf.convert_to_tensor(_EPSILON, np.float32)
+    output = tf.clip_by_value(output, _epsilon, 1 - _epsilon)
+    output = tf.log(output / (1 - output))
+    score = tf.nn.sigmoid_cross_entropy_with_logits(target, output)
+    return K.mean(score, axis=-1)
 
 ########################
 #Custom Image Generator#
@@ -141,10 +149,10 @@ def get_recall(dir, n_samples, dim, model, X, Y, ids):
         match_csv_arr.append(match_csv); templ_csv_arr.append(templ_csv);
         templ_new_arr.append(templ_new); templ_new2_arr.append(templ_new2); maxrad_arr.append(maxr)
 
-    #score = K.binary_crossentropy(tf.convert_to_tensor(Y[0:n_samples],np.float32), tf.convert_to_tensor(Y_pred,np.float32))
-    #print "binary XE score = %f"%K.mean(score, axis=-1)
+#score = K.binary_crossentropy(tf.convert_to_tensor(Y[0:n_samples],np.float32), tf.convert_to_tensor(Y_pred,np.float32))
+#print "binary XE score = %f"%K.mean(score, axis=-1)
     #print "binary XE score = %f"%model.evaluate(X[0:n_samples].astype('float32'), Y[0:n_samples].astype('float32'))
-    print "binary XE score = %f"%binary_crossentropy(Y[0:n_samples],tf.convert_to_tensor(Y_pred,np.float32))
+    print "binary XE score = %f"%binary_crossentropy(Y[0:n_samples],Y_pred)
     print "mean and std of N_match/N_csv (recall) = %f, %f"%(np.mean(match_csv_arr), np.std(match_csv_arr))
     print "mean and std of N_template/N_csv = %f, %f"%(np.mean(templ_csv_arr), np.std(templ_csv_arr))
     print "mean and std of (N_template - N_match)/N_template (fraction of craters that are new) = %f, %f"%(np.mean(templ_new_arr), np.std(templ_new_arr))

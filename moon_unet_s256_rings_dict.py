7d88caebf4bc1a23a13783a7526351d04edac6eb
silburt <silburt@astro.utoronto.ca>
2017-11-09 12:01:49 -0500

    full

--- moon_unet_s256_rings_dict.py
+++ moon_unet_s256_rings_dict.py
@@ -77,13 +77,12 @@ def weighted_binary_cross_entropy(target, output):   #y_true, y_pred
     score = tf.nn.weighted_cross_entropy_with_logits(output,target,pos_weight)
     return K.mean(score, axis=-1)
 
-def binary_crossentropy(target, output):
-    _EPSILON = 10e-8
-    _epsilon = tf.convert_to_tensor(_EPSILON, np.float32)
-    output = tf.clip_by_value(output, _epsilon, 1 - _epsilon)
-    output = tf.log(output / (1 - output))
-    score = tf.nn.sigmoid_cross_entropy_with_logits(target, output)
-    return K.mean(score, axis=-1)
+#https://github.com/fchollet/keras/issues/6261
+def weighted_binary_cross_entropy2(target, output):
+    _epsilon = 10e-8
+    pos_weight = 36.48       #pos_weight imbalance of 1s vs. 0s, hardcoded for ease from training set
+    output = tf.clip_by_value(output, _epsilon, 1. - _epsilon)
+    return - tf.reduce_sum(target * tf.log(output))
 
 ########################
 #Custom Image Generator#
@@ -336,7 +335,7 @@ if __name__ == '__main__':
     MP['lr'] = 0.0001           #learning rate
     MP['bs'] = 8                #batch size: smaller values = less memory but less accurate gradient estimate
     MP['epochs'] = 4            #number of epochs. 1 epoch = forward/back pass through all train data
-    MP['n_train'] = 1000       #number of training samples, needs to be a multiple of batch size. Big memory hog.
+    MP['n_train'] = 30000       #number of training samples, needs to be a multiple of batch size. Big memory hog.
     MP['n_valid_recall'] = 1000 #number of examples to calculate recall on after each epoch. Expensive operation.
     MP['n_test_recall'] = 5000  #number of examples to calculate recall on after training. Expensive operation.
     MP['inv_color'] = 0         #use inverse color

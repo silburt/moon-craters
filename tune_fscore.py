faf35dca92eaa8d848d8e23198e6d8d84b96a397
silburt <silburt@astro.utoronto.ca>
2017-11-28 09:34:46 -0500

    tuned hypers

--- tune_fscore.py
+++ tune_fscore.py
@@ -5,24 +5,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import glob
 
-beta = 10 #controls precision's weakness in f_beta score
-real_data = 0
+beta = 1 #controls precision's weakness in f_beta score
+real_data = 1
 
 f_beta, precision, recall, ext = [], [], [], ''
 if real_data == 1:
     dir = 'tune_jobs'
     files = glob.glob('%s/*.txt'%dir)
-    for f in files:
+    for file in files:
         try:
-            lines = open(f,'r').readlines()
+            lines = open(file,'r').readlines()
             p = float(lines[-3].split('=')[1].split(',')[0])
             r = float(lines[-4].split('=')[1].split(',')[0])
             f = round((1+beta**2)*(r*p)/(p*beta**2 + r),3)
             precision.append(p)
             recall.append(r)
             f_beta.append(f)
+            
+            if f > 0.7:
+                temp = lines[-5].replace('\n','').replace('=', ', ').split(',')
+                #print file
+                print "fscore=%f,llt2=%f, rt=%f, temp_thresh=%f, targ_thresh=%f"%(f,float(temp[1]),float(temp[3]),float(temp[5]),float(temp[7]))
+        
         except:
-            print "%s not done"%f
+            pass
+            #print "%s not done"%f
 else:
     p = np.linspace(0.1,1,20)
     params = list(itertools.product(*[p,p]))

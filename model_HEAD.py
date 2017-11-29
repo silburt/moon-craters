55198b9adab97f73307dd7be2f000c1aa5b759fb
silburt <silburt@astro.utoronto.ca>
2017-11-28 20:26:15 -0500

    edit

--- model_HEAD.py
+++ model_HEAD.py
@@ -294,11 +294,15 @@ if __name__ == '__main__':
 #    MP['dropout'] = [0.15]
 
     #example for iterating over parameters
-    MP['N_runs'] = 4
-#    MP['lambda']=[1e-4,1e-4,1e-5,1e-5]              #regularization
-#    MP['dropout']=[0.25,0.15,0.25,0.15]             #dropout after merge layers
-    MP['lambda']=[1e-6,1e-6,1e-7,1e-7]              #regularization
-    MP['dropout']=[0.25,0.15,0.25,0.15]             #dropout after merge layers
+    MP['N_runs'] = 2
+    MP['lambda']=[1e-4,1e-4]              #regularization
+    MP['dropout']=[0.25,0.15]             #dropout after merge layers
+#    MP['lambda']=[1e-5,1e-5]              #regularization
+#    MP['dropout']=[0.25,0.15]             #dropout after merge layers
+#    MP['lambda']=[1e-6,1e-6]              #regularization
+#    MP['dropout']=[0.25,0.15]             #dropout after merge layers
+#    MP['lambda']=[1e-7,1e-7]              #regularization
+#    MP['dropout']=[0.25,0.15]             #dropout after merge layers
 
     #run models
     get_models(MP)

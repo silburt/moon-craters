fixed finally crater match target precision issue

utils/template_match_target.py
@@ -72,7 +72,7 @@ def template_match_target(target, minrad=minrad_, maxrad=maxrad_, longlat_thresh
     return coords
 
 
-def template_match_target_to_csv(target, csv, minrad=minrad_, maxrad=maxrad_, longlat_thresh2=longlat_thresh2_, rad_thresh=rad_thresh_, template_thresh=template_thresh_, target_thresh=target_thresh_, remove_large_craters_csv=0):
+def template_match_target_to_csv(target, csv_coords, minrad=minrad_, maxrad=maxrad_, longlat_thresh2=longlat_thresh2_, rad_thresh=rad_thresh_, template_thresh=template_thresh_, target_thresh=target_thresh_, remove_large_craters_csv=0):
 
     # get coordinates from template matching
     templ_coords = template_match_target(target, minrad, maxrad, longlat_thresh2, rad_thresh, template_thresh, target_thresh)
@@ -80,19 +80,7 @@ def template_match_target_to_csv(target, csv, minrad=minrad_, maxrad=maxrad_, lo
     # find max detected crater radius
     maxr = 0
     if len(templ_coords > 0):
-        x,y,r = templ_coords.T
-        maxr = np.max(r)
-    
-    # If remove_large_craters_csv == 1, see how recall improves when large craters are excluded.
-    if remove_large_craters_csv == 1:
-        index = np.where((csv.T[2] <= maxr+0.2))
-        if len(index[0]) > 0:
-            csv_coords = csv[index]
-        else:
-            print("all craters are larger than max detected radius")
-            csv_coords = csv
-    else:
-        csv_coords = csv
+        maxr = np.max(templ_coords.T[2])
 
     # compare template-matched results to ground truth csv input data
     N_match = 0
@@ -112,10 +100,19 @@ def template_match_target_to_csv(target, csv, minrad=minrad_, maxrad=maxrad_, lo
                 print(csv_coords[id])
                 if i > 0:                               #keep only first match as true
                     index[id] = False
+        elif N == 1:
+            print lo,la,r
+            print csv_coords[index]
+            print ''
         N_match += min(1,N)                             #count up to one match in recall
         csv_coords = csv_coords[np.where(index==False)] #remove csv so it can't be re-matched again
         if len(csv_coords) == 0:
             break
+
+    if remove_large_craters_csv == 1:
+        N_large_unmatched = len(np.where(csv_coords.T[2] > maxr)[0])
+        if N_large_unmatched < N_csv:
+            N_csv -= N_large_unmatched
 
     return N_match, N_csv, N_templ, maxr, csv_duplicate_flag
 
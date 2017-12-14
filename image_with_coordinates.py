5dadfbfd714a94a332acf07fc443176612abbbc2
silburt <silburt@astro.utoronto.ca>
2017-12-14 10:13:17 -0500

    checking preds

---
+++ image_with_coordinates.py
@@ -0,0 +1,19 @@
+import h5py
+import numpy as np
+
+def get_id(i, zeropad=5):
+    return 'img_{i:0{zp}d}'.format(i=i, zp=zeropad)
+
+dtype = 'dev'
+data_dir = '/scratch/m/mhvk/czhu/moondata/final_data/%s_images.hdf5'%dtype
+pred_dir = 'datasets/HEAD/HEAD_%spreds_n30000_final.hdf5'%dtype
+
+xleft, xright, ylow, yhi = -39,-38,-20,-19
+
+data = h5py.File(data_dir, 'r')
+for i in range(1000):
+    llbd = data['longlat_bounds'][get_id(i)][...]
+    rawlen = data['pix_bounds'][get_id(i)][2] - data['pix_bounds'][get_id(i)][0]
+    if xleft > llbd[0] and xright < llbd[1] and ylow > llbd[2] and yhigh < llbd[3] and rawlen > 3000:
+        print(i,llbd)
+

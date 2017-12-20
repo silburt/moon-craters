3cd71412e8ad99a9a4132ef00514affa4f9ab144
silburt <silburt@astro.utoronto.ca>
2017-12-20 00:02:31 -0500

    edit

--- tune_craterdetect_hypers_HEAD.py
+++ tune_craterdetect_hypers_HEAD.py
@@ -40,7 +40,7 @@ def get_recall(preds, csvs, nimgs, longlat_thresh2, template_thresh, rad_thresh)
                                                                                          longlat_thresh2=longlat_thresh2,
                                                                                          template_thresh=template_thresh,
                                                                                          rad_thresh=rad_thresh,
-                                                                                         remove_largesmall_csvs=1)
+                                                                                         rmv_oob_csvs=1)
         if N_match > 0:
             print(i, N_match, N_csv, N_templ, maxr, csv_duplicates)
             p = float(N_match)/float(N_match + (N_templ-N_match))   #assuming all unmatched detected circles are false positives
